a4ed920d3b93de1bd57f0c13ac136aa42e87bad4
Jialin Cai <jialin.cai@icloud.com>
2024-08-03 22:33:28 -0400

    nit: file restructure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
