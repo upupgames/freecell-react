5da456e144c9d19cf51d7e7c56a53f17f9c56d61
HeyItsDavis <HeyItsDavis@users.noreply.github.com>
2024-08-04 18:21:00 -0700

    Changed cards to use numbers for rank and an enum for suit

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,66 @@
 import React from 'react';
 import styles from '@styles/Card.module.css';
 
+export enum Suit {
+  Clubs = 0,
+  Diamonds = 1,
+  Hearts = 2,
+  Spades = 3
+}
+
 type CardProps = {
-  suit: string;
-  rank: string;
+  suit: Suit;
+  rank: number;
 };
 
 const Card: React.FC<CardProps> = ({ suit, rank }) => {
+  const rankDisplay = () => {
+    switch (rank) {
+      case 1:
+        return 'A';
+      case 11:
+        return 'J'
+      case 12:
+        return 'Q'
+      case 13:
+        return 'K'
+      default:
+        return rank.toString();
+    }
+  }
+
+  const suitDisplay = () => {
+    switch (suit) {
+      case Suit.Clubs:
+        return '♣';
+      case Suit.Diamonds:
+        return '♦';
+      case Suit.Hearts:
+        return '♥';
+      case Suit.Spades:
+        return '♠';
+      default:
+        return '';
+    }
+  }
+
+  const suitClass = () => {
+    switch (suit) {
+      case Suit.Diamonds:
+      case Suit.Hearts:
+        return styles.cardSuitRed;
+      case Suit.Clubs:
+      case Suit.Spades:
+        return styles.cardSuitBlack;
+      default:
+        return '';
+    }
+  }
+
   return (
     <div className={styles.card}>
-      {rank} of {suit}
+      <div className={styles.cardRank}>{rankDisplay()}</div>
+      <div className={suitClass()}>{suitDisplay()}</div>
     </div>
   );
 };
