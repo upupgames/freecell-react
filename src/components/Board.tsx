9eea963147f7b6061a732307b2878c8a16d44292
HeyItsDavis <60993451+HeyItsDavis@users.noreply.github.com>
2024-08-04 19:31:02 -0700

    Improve card data and visualization (#4)
    * Changed cards to use numbers for rank and an enum for suit
    
    * Updated CSS for the cards to make them more card-like
    
    * Updated board to be compatable with new card
    
    * Card rank and suit are properly displayed at the top of the card
    
    ---------
    
    Co-authored-by: HeyItsDavis <HeyItsDavis@users.noreply.github.com>

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Card from '@components/Card';
+import Card, {Suit} from '@components/Card';
 import styles from '@styles/Board.module.css';
 
 const Board: React.FC = () => {
   return (
     <div className={styles.board}>
-        <Card suit="Hearts" rank="A" />
-        <Card suit="Diamonds" rank="2" />
-        <Card suit="Clubs" rank="3" />
-        <Card suit="Spades" rank="4" />
+        <Card suit={Suit.Hearts} rank={1} />
+        <Card suit={Suit.Diamonds} rank={2} />
+        <Card suit={Suit.Clubs} rank={3}/>
+        <Card suit={Suit.Spades} rank={4} />
     </div>
   );
 };
